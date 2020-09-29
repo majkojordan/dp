aca89e4ea67075948d7072c719824f2a6b2300db
Marek Snincak <m.snincak@gmail.com>
2020-09-29 23:08:32 +0100

    add session_id column

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from config import DB_CONNECTION_STRING
+from datetime import timedelta
 
 
-def load_data(count="10000", event_type="view_item"):
+def load_data(count="1000", event_type="view_item"):
     query = f"SELECT * FROM events WHERE event_type = '{event_type}' ORDER BY timestamp DESC LIMIT {count}"
     return pd.read_sql(query, DB_CONNECTION_STRING)
     try:
@@ -11,13 +12,29 @@ def load_data(count="10000", event_type="view_item"):
         return None
 
 
-df = load_data()
+def add_sessions(df):
+    df = df.sort_values(by=["customer_id", "timestamp"])
 
-most_popular = (
-    df.groupby(["customer_id", "product_id"])
-    .agg({"customer_id": "first", "product_id": "first"})["product_id"]
-    .value_counts()
-    .head(10)
-    .keys()
-    .tolist()
-)
+    df["session_id"] = (
+        (df["customer_id"] != df["customer_id"].shift())
+        | (df["timestamp"] - df["timestamp"].shift() > pd.Timedelta(hours=1))
+    ).cumsum()
+
+    return df
+
+
+def get_popular_items(df, count=10):
+    most_popular = (
+        df.groupby(["customer_id", "product_id"])
+        .agg({"customer_id": "first", "product_id": "first"})["product_id"]
+        .value_counts()
+        .head(count)
+        .keys()
+        .tolist()
+    )
+
+    return most_popular
+
+
+df = load_data(1000)
+df = add_sessions(df)
